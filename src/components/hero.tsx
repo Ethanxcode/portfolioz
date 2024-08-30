fa690dd24c4958c8673a3f1f0ee5d538e4e6354b
Ethan <trannguyendanhuy2904@gmail.com>
2024-08-31 02:51:12 +0700

    second

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,25 +1,34 @@
-import Image from "next/image";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
-import Link from "next/link";
-import { Button } from "./ui/button";
-import { AtSign, Github, Linkedin } from "lucide-react";
-import { Reveal } from "./custom/reveal";
-import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
-import me from "@/lib/img/me.png";
+import Image from 'next/image';
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from './ui/tooltip';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { AtSign, Github, Linkedin } from 'lucide-react';
+import { Reveal } from './custom/reveal';
+import { TypewriterEffectSmooth } from './ui/typewriter-effect';
+import me from '@/lib/img/me.jpg';
 
 export default function Hero() {
-    const name = [
-        { text: "Daniel" },
-        { text: "Wan" },
-    ]
+    const name = [{ text: 'Ethan' }, { text: 'Tran' }];
 
     return (
-        <header id="hero" className="flex flex-col sm:flex-row h-screen w-full justify-center items-center gap-4 mt-16">
+        <header
+            id="hero"
+            className="flex flex-col sm:flex-row h-screen w-full justify-center items-center gap-4 mt-16"
+        >
             <div className="flex flex-col gap-2 w-full">
                 <TypewriterEffectSmooth words={name} />
                 <Reveal
                     initial={{ opacity: 0, x: 50 }}
-                    whileInView={{ opacity: 1, x: 0, transition: { duration: 0.4 } }}
+                    whileInView={{
+                        opacity: 1,
+                        x: 0,
+                        transition: { duration: 0.4 },
+                    }}
                 >
                     <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
                         Software Engineer
@@ -27,21 +36,33 @@ export default function Hero() {
                 </Reveal>
                 <Reveal
                     initial={{ opacity: 0, x: 50 }}
-                    whileInView={{ opacity: 1, x: 0, transition: { duration: 0.4, delay: 0.2 } }}
+                    whileInView={{
+                        opacity: 1,
+                        x: 0,
+                        transition: { duration: 0.4, delay: 0.2 },
+                    }}
                 >
                     <p className="leading-7">
-                        I plan, design, build, test and deploy software. With the help of coffee of course.
+                        I plan, design, build, test and deploy software. With
+                        the help of coffee of course.
                     </p>
                 </Reveal>
                 <div className="flex gap-4">
                     <TooltipProvider>
                         <Reveal
                             initial={{ opacity: 0, y: 20 }}
-                            whileInView={{ opacity: 1, y: 0, transition: { duration: 0.4 } }}
+                            whileInView={{
+                                opacity: 1,
+                                y: 0,
+                                transition: { duration: 0.4 },
+                            }}
                         >
                             <Tooltip>
                                 <TooltipTrigger asChild>
-                                    <Link href="https://www.linkedin.com/in/danielwanjo/" target="_blank">
+                                    <Link
+                                        href="https://www.linkedin.com/in/trannguyendanghuy/"
+                                        target="_blank"
+                                    >
                                         <Button variant="secondary" size="icon">
                                             <Linkedin />
                                         </Button>
@@ -54,11 +75,18 @@ export default function Hero() {
                         </Reveal>
                         <Reveal
                             initial={{ opacity: 0, y: 20 }}
-                            whileInView={{ opacity: 1, y: 0, transition: { duration: 0.4, delay: 0.2 } }}
+                            whileInView={{
+                                opacity: 1,
+                                y: 0,
+                                transition: { duration: 0.4, delay: 0.2 },
+                            }}
                         >
                             <Tooltip>
                                 <TooltipTrigger asChild>
-                                    <Link href="https://github.com/KeluhingBavui" target="_blank">
+                                    <Link
+                                        href="https://github.com/Ethanxcode"
+                                        target="_blank"
+                                    >
                                         <Button variant="secondary" size="icon">
                                             <Github />
                                         </Button>
@@ -71,11 +99,18 @@ export default function Hero() {
                         </Reveal>
                         <Reveal
                             initial={{ opacity: 0, y: 20 }}
-                            whileInView={{ opacity: 1, y: 0, transition: { duration: 0.4, delay: 0.4 } }}
+                            whileInView={{
+                                opacity: 1,
+                                y: 0,
+                                transition: { duration: 0.4, delay: 0.4 },
+                            }}
                         >
                             <Tooltip>
                                 <TooltipTrigger asChild>
-                                    <Link href="mailto:daniel01.jonathan@gmail.com" target="_blank">
+                                    <Link
+                                        href="mailto:trannguyendanhuy2904@gmail.com"
+                                        target="_blank"
+                                    >
                                         <Button variant="secondary" size="icon">
                                             <AtSign />
                                         </Button>
@@ -94,7 +129,7 @@ export default function Hero() {
                     src={me}
                     alt="Me"
                     fill
-                    style={{ objectFit: "contain" }}
+                    style={{ objectFit: 'contain' }}
                     className="rounded-md object-cover"
                 />
             </div>
